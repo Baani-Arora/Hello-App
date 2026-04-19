14b0f2edb213c4addb8e06b488641474e9583634
Baani Arora <your_github_email@example.com>
2026-04-20 00:28:17 +0530

    UC4: Display Hello with comma-separated names

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -3,9 +3,14 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+            String result = "";
             for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello, " + args[i] + "!");
+                result += args[i];
+                if (i < args.length - 1) {
+                    result += ", ";
+                }
             }
+            System.out.println("Hello, " + result + "!");
         }
     }
 }
